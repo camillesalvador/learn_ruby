302d66efc83ddf3876ebc16b4a674019a740d729
Camille Salvador <camille.salvador@outlook.com>
2017-09-11 23:22:40 -0600

    temperature and calculator solutions

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,8 +1,8 @@
 #write your code here
 def ftoc(temp)
-  (temp - 32) * (5.0/9.0)
+  (temp - 32) * (100.0/180.0)
 end
 
 def ctof(temp)
-  32 + (temp * 5.0/9.0)
+  32 + temp * (180.0/100.0)
 end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,45 +1,23 @@
-#write your code here
 def add(a, b)
   a + b
 end
 
 def subtract(a, b)
-  a - b
+    a - b
 end
 
 def sum(arr)
-  total
-  arr.each do |val|
-    total += val
+  num = 0
+  arr.each do |item|
+    num += item
   end
-  total
+  num
 end
 
-def multiply(a, b) 
+def multiply(a, b)
   a * b
 end
 
-def multiply(arr)
-  product
-  arr.each do |val|
-    product *= val
-  end
-  product
-end
-
 def power(a, b)
-  Math.pow(a, b)
-end
-
-def factorial(x)
-  if x == 0
-		return 1
-	else
-		temp = 1
-		while x > 1 do
-      temp *= x
-			x--
-		end
-		return temp
-	end
+  a**b
 end
